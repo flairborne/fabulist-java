96045f9092aa1cee92bfb14ddf2d540ef11d3f28
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-04-15 18:13:43 +0800

    feat(element)!: remove simple message
    BREAKING CHANGE: delete SimpleMessage record

--- element/src/main/java/com/flairborne/fabulist/element/channel/message/SimpleMessage.java
+++
@@ -1,9 +0,0 @@
-package com.flairborne.fabulist.element.channel.message;
-
-public record SimpleMessage (String type) implements Message {
-
-    @Override
-    public String type() {
-        return type;
-    }
-}

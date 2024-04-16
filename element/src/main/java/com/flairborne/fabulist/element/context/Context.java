c46160eb12c5e7868aa4249915b0829a3988bd45
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-04-16 21:05:33 +0800

    feat(element): add context predicate is property true helper method

--- element/src/main/java/com/flairborne/fabulist/element/context/Context.java
+++ element/src/main/java/com/flairborne/fabulist/element/context/Context.java
@@ -1,9 +1,15 @@
 package com.flairborne.fabulist.element.context;
 
+import java.util.function.Predicate;
+
 /**
  * Context refers to where a {@link Runtime runtime} is situated in such as a command-line, web application, etc.
  */
 public interface Context {
 
     PropertyMap properties();
+
+    static Predicate<Context> isPropertyTrue(String property) {
+        return context -> context.properties().getBoolean(property);
+    }
 }

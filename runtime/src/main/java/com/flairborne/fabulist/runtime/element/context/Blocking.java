ee9ba9ece5bb2edd1b4bca3a3549d5c99c91c517
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2024-06-07 17:17:11 +0800

    docs(runtime): improve blocking interface docs

--- runtime/src/main/java/com/flairborne/fabulist/runtime/element/context/Blocking.java
+++ runtime/src/main/java/com/flairborne/fabulist/runtime/element/context/Blocking.java
@@ -1,10 +1,14 @@
 package com.flairborne.fabulist.runtime.element.context;
 
 /**
- * An element is blocking when it requires a stimulus (like a choice made by a player) in order to proceed.
+ * An element is blocking when it requires a stimulus (like a choice made by a player) in order to
+ * proceed the {@link Runtime runtime}.
  */
 public interface Blocking {
 
+    /**
+     * @return whether this element is capable of blocking the {@link Runtime runtime}
+     */
     default boolean isBlocking() {
         return true;
     }
